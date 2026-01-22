e0673c19ef3b7c9a751603ba444071d32857a8bb
themoviesite90 <themoviesite90@gmail.com>
2026-01-23 03:49:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-.
+const API_KEY = "PUT_YOUR_API_KEY_HERE";
+
+const IMG = "https://image.tmdb.org/t/p/w500";
+const TRENDING = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`;
+const SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
+const moviesDiv = document.getElementById("movies");
+const searchInput = document.getElementById("search");
+
+// Load trending first
+fetch(TRENDING)
+  .then(res => res.json())
+  .then(data => showMovies(data.results));
+
+function showMovies(movies) {
+  moviesDiv.innerHTML = "";
+
+  movies.forEach(movie => {
+    if (!movie.poster_path) return;
+
+    const div = document.createElement("div");
+    div.className = "movie";
+
+    div.innerHTML = `
+      <img src="${IMG + movie.poster_path}">
+      <p>${movie.title}</p>
+    `;
+
+    moviesDiv.appendChild(div);
+  });
+}
+
+// Search
+searchInput.addEventListener("keyup", function () {
+  const value = searchInput.value;
+
+  if (value.trim() === "") {
+    fetch(TRENDING)
+      .then(res => res.json())
+      .then(data => showMovies(data.results));
+  } else {
+    fetch(SEARCH + value)
+      .then(res => res.json())
+      .then(data => showMovies(data.results));
+  }
+});
