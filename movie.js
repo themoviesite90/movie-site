45b368c102e35d572fb7bed60cae8a6db14f196a
themoviesite90 <themoviesite90@gmail.com>
2026-01-23 23:06:29 +0600

    Update movie.js

--- movie.js
+++ movie.js
@@ -42,8 +42,6 @@ document.getElementById("trailerBtn").onclick = () => {
         alert("No trailer found!");
       }
     });
-};// Add to movie.js after trailer button code
-// Favorite functionality
 function checkFavorite() {
   const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
   const isFav = favorites.some(m => m.id == id && m.type == type);
