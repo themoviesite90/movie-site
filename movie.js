b985e814bb7675e96529f90c5668a16d4b8f125b
themoviesite90 <themoviesite90@gmail.com>
2026-01-24 03:43:11 +0600

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,14 +1,9 @@
 const API_KEY = "b9864cdbbdcef170f412314e777c14f5";
 
-// Read URL params
-const params = new URLSearchParams(window.location.search);
+// Get URL params
+const params = new URLSearchParams(location.search);
 const id = params.get("id");
 const type = params.get("type") || "movie";
-
-if (!id) {
-  document.body.innerHTML = "No movie ID found in URL!";
-  throw new Error("No ID");
-}
 
 // Elements
 const titleEl = document.getElementById("movieTitle");
@@ -23,37 +18,29 @@ fetch(`https://api.themoviedb.org/3/${type}/${id}?api_key=${API_KEY}`)
   .then(res => res.json())
   .then(movie => {
     titleEl.innerText = movie.title || movie.name;
-    overviewEl.innerText = movie.overview;
-    ratingEl.innerText = movie.vote_average;
+    overviewEl.innerText = movie.overview || "No description.";
+    ratingEl.innerText = movie.vote_average || "N/A";
+    genresEl.innerText = movie.genres?.map(g => g.name).join(", ") || "N/A";
 
-    genresEl.innerText = movie.genres.map(g => g.name).join(", ");
-
-    loadTrailer(id);
+    // Load YouTube trailer
+    return fetch(`https://api.themoviedb.org/3/${type}/${id}/videos?api_key=${API_KEY}`);
   })
-  .catch(err => {
-    document.body.innerHTML = "Error loading movie data";
-    console.error(err);
-  });
-
-// Load trailer
-function loadTrailer(id) {
-  fetch(`https://api.themoviedb.org/3/${type}/${id}/videos?api_key=${API_KEY}`)
-    .then(res => res.json())
-    .then(data => {
-      const trailer = data.results.find(v => v.site === "YouTube");
-      if (!trailer) {
-        watchBtn.innerText = "No trailer found";
-        return;
-      }
-
+  .then(res => res.json())
+  .then(videoData => {
+    const trailer = videoData.results.find(v => v.site === "YouTube");
+    if (trailer) {
       watchBtn.onclick = () => {
         playerDiv.innerHTML = `
-          <iframe width="100%" height="400"
-            src="https://www.youtube.com/embed/${trailer.key}"
-            frameborder="0"
-            allowfullscreen>
+          <iframe width="100%" height="400" 
+            src="https://www.youtube.com/embed/${trailer.key}" 
+            frameborder="0" allowfullscreen>
           </iframe>
         `;
       };
-    });
-}
+    } else {
+      watchBtn.innerText = "Trailer not available";
+    }
+  })
+  .catch(err => {
+    console.error("Movie load error:", err);
+  });
